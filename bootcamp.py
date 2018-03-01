a1e598825ff5c849bad145e7922d20ad1e502380
skvrahul <skvrahul@gmail.com>
2018-03-01 17:26:59 +0530

    Adds helper module

---
+++ bootcamp.py
@@ -0,0 +1,36 @@
+# imports
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+#Returns an Email server which we can use to send emails
+def create_server():
+    username = 'bootcamp_manipal@outlook.com'
+    password = input('Enter password: ')
+
+    s = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
+    s.starttls()
+    s.login(username, password)
+    return s
+
+#Returns an Email server which we can use to send emails
+def send_email(to, body, server):
+    username = 'bootcamp_manipal@outlook.com'
+        
+
+    message = MIMEMultipart()
+    message['From'] = username
+    message['To'] = to
+    message['Subject'] = "TEST"
+    message.attach(MIMEText(body, 'plain'))
+    text = message.as_string()
+    server.sendmail(username, to, text)
+    print('Successfully sent mail to ', to)
+
+def close_server(server):
+    server.close()
+
+
+    #TODO
+def read_file(path):
+    pass
